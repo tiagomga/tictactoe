88a40a1364f9bd028942eb54b1652c16d64e2016
Tiago Antão <109687852+tiagomga@users.noreply.github.com>
2025-01-20 23:35:43 +0000

    Create game logic

---
+++ script.js
@@ -0,0 +1,69 @@
+const gameController = (function() {
+    const gameBoard = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+
+    function checkRows() {
+        for (let row = 0; row < gameBoard.length; row++) {
+            for (let col = 0; col < gameBoard[row].length-1; col++) {
+                if (
+                    gameBoard[row][col] !== 0
+                    && gameBoard[row][col] === gameBoard[row][col+1]
+                ) {
+                    return true; 
+                }
+            }
+        }
+        return false;
+    };
+
+    function checkColumns() {
+        for (let col = 0; col < gameBoard.length; col++) {
+            for (let row = 0; row < gameBoard.length-1; row++) {
+                if (
+                    gameBoard[row][col] !== 0
+                    && gameBoard[row][col] === gameBoard[row+1][col]
+                ) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    };
+
+    function checkDiagonals() {
+        if (
+            gameBoard[1][1] !== 0
+            && gameBoard[0][0] === gameBoard[1][1]
+            && gameBoard[1][1] === gameBoard[2][2]
+        ) {
+            return true;
+        } else if (
+            gameBoard[1][1] !== 0
+            && gameBoard[2][0] === gameBoard[1][1]
+            && gameBoard[1][1] === gameBoard[0][2]
+        ) {
+            return true;
+        }
+        return false;
+    };
+
+    function isGameOver() {
+        if (checkRows() || checkColumns() || checkDiagonals()) {
+            return true;
+        }
+        return false;
+    };
+
+    function play(player, row, column) {
+        if (gameBoard[row][column] === 0) {
+            gameBoard[row][column] = player.id;
+        }
+    };
+
+    return { gameBoard, isGameOver, play };
+})();
+
+function printGameBoard(gameBoard) {
+    for (let row of gameBoard) {
+        console.log(row);
+    }
+}
